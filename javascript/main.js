670457087229db8b2d2175b9424845e5a3d4d31e
rriiaad <85185705+rriiaad@users.noreply.github.com>
2021-12-09 10:59:46 +0000

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+document.querySelector(".submit").addEventListener("click", () => {
+  let text = document.querySelector("#filter").value;
+  console.log(text);
+});
+
+let jobs = document.querySelector(".jobsContainer");
